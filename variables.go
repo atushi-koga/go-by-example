4b8b8f3d8b9a44f82528363c37a19345aa147e82
atsushi.koga <stbe51@gmail.com>
2020-04-19 16:42:47 +0900

    出力結果をコメントで記載

--- variables.go
+++ variables.go
@@ -6,24 +6,24 @@ func main(){
 	// 変数の宣言と初期化
 	// 変数を初期化すれば、Goは変数の型を推測する
 	var a = "initial"
-	fmt.Println(a)
+	fmt.Println(a)	// initial
 
 	// 複数の変数を一度に宣言
 	var b, c = 1, 2
-	fmt.Println(b, c)
+	fmt.Println(b, c)	// 1 2
 
 	// 明示的に初期化していない変数の値はその型のゼロ値
 	var d bool
-	fmt.Println(d)
+	fmt.Println(d)	// false
 
 	var e int
-	fmt.Println(e)
+	fmt.Println(e)	// 0
 
 	// 宣言と初期化を行う省略記法
-	f := "short"	// var f string = "short
-	fmt.Println(f)
+	f := "short"
+	fmt.Println(f)	// short
 
 	// 上の省略記法は以下と同じ
 	var g string = "long"
-	fmt.Println(g)
+	fmt.Println(g)	// long
 }
