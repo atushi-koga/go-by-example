b1083ba22e258b601dfc0a2e5e7ff36dbe2354c1
koga <koga@wh-plus.com>
2024-11-15 14:14:57 +0900

    test:テストコード追加

--- sample_test.go
+++ sample_test.go
@@ -22,3 +22,10 @@ func TestSample7(t *testing.T) {
 		t.Errorf("Sample3() = %v, want 3", got)
 	}
 }
+
+func TestSample8(t *testing.T) {
+	got := sample8()
+	if got != 8 {
+		t.Errorf("Sample8() = %v, want 8", got)
+	}
+}
